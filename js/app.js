ff1acee62422c85718747dd60ed50efb0fa047e4
jenniferjara <lisbet.jara@gmail.com>
2016-09-22 16:29:45 -0500

    cambios en la version tres

--- js/app.js
+++ js/app.js
@@ -3,23 +3,47 @@ window.addEventListener("load", function (){
 	var boton = document.getElementById("btn");
 
 	boton.addEventListener("click", function(){
-		newForm();
+		newElement();
 	});
-	function newForm (){
-		btn.className = "dNone";
+	function newElement (){
 		var form = document.createElement("form");
 		var input = document.createElement("input");
-		var btnGreen = document.createElement("button")
-		var text = document.createTextNode("Guardar");
-		form.className = "form";
-		input.className = "input";
-		input.setAttribute("placeholder", "Añadir una lista...");
-		btnGreen.className = "btnGreen";
-		btnGreen.setAttribute("type", "submit");
-		btnGreen.setAttribute("id", "btnDos")
-		contenedor.insertBefore(form, contenedor.childNodes[0]);
-		form.insertBefore(input, form.childNodes[0]);
-		form.insertBefore(btnGreen,form.childNodes[1]);
-		btnGreen.appendChild(text);
+		var btnGreen = document.createElement("button");
+
+		function newForm(){
+			btn.className = "dNone";
+			form.className = "form";
+			input.className = "input";
+			input.setAttribute("placeholder", "Añadir una lista...");
+			btnGreen.className = "btnGreen";
+			btnGreen.setAttribute("id", "btnDos")
+			contenedor.insertBefore(form, contenedor.childNodes[0]);
+			form.insertBefore(input, form.childNodes[0]);
+			form.insertBefore(btnGreen,form.childNodes[1]);
+			var text = document.createTextNode("Guardar");
+			btnGreen.appendChild(text);
+		}
+		newForm();
+
+		function newTarjeta(){
+			btnGreen.addEventListener("click", function(e){
+				e.preventDefault();
+				form.className = "dNone";
+
+				var title = document.createElement("div");
+				title.innerText = input.value;
+				title.setAttribute("class", "list-header")
+				contenedor.insertBefore(title, contenedor.childNodes[0]);
+
+				var link = document.createElement("a");
+				link.innerText = "Anadir otra tarjeta...";
+				link.setAttribute("class", "link");
+				link.setAttribute("href", "#");
+
+				contenedor.insertBefore(link, contenedor.childNodes[1]);
+			});
+		}
+		newTarjeta();
 	}
+
 });
--- readme.md
+++ readme.md
@@ -1,5 +1,14 @@
-#TRELLO
+#EJERCICIO: TRELLO
 
 ##VERSIÓN 0.0.1
 
 1. Un elemento en el HTML con el mensaje "Añadir una lista", que al dar click muestre un input y un botón (formulario) para que el usuario ingrese el nombre de la lista.
+
+##Versión 0.0.2
+
+1. Mostrar en el HTML, el texto ingresado al dar click en el botón de "Guardar" del formulario (como si fuera título de la lista).
+2. Debajo del título, mostrar el mensaje clickeable de "Añadir una tarjea".
+
+##VERSIÓN 0.0.3
+
+1. Una vez agregada la lista, mostrar el mensaje clickeable de "Añadir una lista" al lado de la lista agregada.
