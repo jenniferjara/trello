2f76a97f7bd88ac528171be37c0a422d154baa08
jenniferjara <lisbet.jara@gmail.com>
2016-10-06 16:05:24 -0500

    Implementando el evento Drag & Drop

--- js/app.js
+++ js/app.js
@@ -1,84 +1,109 @@
-window.addEventListener("load", function (){
-	var cajaGrande = document.getElementById("cajaGrande");
-	var caja = document.getElementById("caja");
-	var span = document.getElementById("span");
-	var form = document.getElementById("form");
-	var input = document.getElementById("input");
-	var btnGreen = document.getElementById("btnGreen");
+window.addEventListener("load", cargaPagina);
+var cajaGrande = document.getElementById("cajaGrande");
+var caja = document.getElementById("caja");
+var span = document.getElementById("span");
+var form = document.getElementById("form");
+var input = document.getElementById("input");
+var btnGreen = document.getElementById("btnGreen");
+var contador = 1;
 
-	span.addEventListener("click", function(e){
-		e.preventDefault();
-		newForm();
-	});
+function cargaPagina(){
+	span.addEventListener("click", newForm);
+}
+function newForm(e){
+	e.preventDefault();
+	span.className = "dNone";
+	form.className = "form";
+	input.focus();
+	input.value = "";
+}
 
-	function newForm(){
-		span.className = "dNone";
-		form.className = "form";
-		input.focus();
-		input.value = "";
-	}
+btnGreen.addEventListener("click", newTarjeta);
+btnGreen.addEventListener("click", nuevaCaja);
+caja.addEventListener("dragover", arrastrarSobre);
+caja.addEventListener("drop", soltar);
 
-	btnGreen.addEventListener("click", function(e){
-		e.preventDefault();
-		form.className = "dNone";
-		newTarjeta();
-		nuevaCaja();
-	});
+function newTarjeta(e){
+	e.preventDefault();
+	form.className = "dNone";
+	var padre = btnGreen.parentElement.parentElement;
+	var title = document.createElement("div");
+	var link = document.createElement("a");
 
-	function newTarjeta(){
-		var padre = btnGreen.parentElement.parentElement;
-		var title = document.createElement("div");
-		title.innerText = input.value;
-		title.setAttribute("class", "list-header")
-		padre.insertBefore(title, padre.childNodes[0]);
+	title.innerText = input.value;
+	title.classList.add("list-header");
+	padre.insertBefore(title, padre.childNodes[0]);
+	
+	link.innerText = "Anadir una tarjeta...";
+	padre.appendChild(link);
+	link.classList.add("link");
 
-		var link = document.createElement("a");
-		link.innerText = "Anadir una tarjeta...";
-		padre.appendChild(link);
-		link.classList.add("link");
+	link.addEventListener("click", newText);
+}
+function nuevaCaja() {
+	var cajaDos = document.createElement("div");
+	cajaDos.className = "caja";
+	cajaGrande.appendChild(cajaDos);
+	span.className = "btn";
+	cajaDos.insertBefore(span, cajaDos.childNodes[0]);
+	cajaDos.insertBefore(form, cajaDos.childNodes[1]);
 
-		link.addEventListener("click", function(){
-			link.className = "dNone";
-			newText(padre);
-		});
-	}
+	cajaDos.addEventListener("dragover", arrastrarSobre);
+	cajaDos.addEventListener("drop", soltar);
+}
+function newText(){
+	this.className = "dNone";
+	var newForm = document.createElement("form");
+	var textArea = document.createElement("textarea");
+	var btnSmall = document.createElement("button");
+	
+	newForm.insertBefore(textArea, newForm.childNodes[0]);
+	newForm.insertBefore(btnSmall, newForm.childNodes[1]);
+	this.parentElement.appendChild(newForm);
+	btnSmall.innerText = "Añadir";
+	btnSmall.type = "button";
+	btnSmall.classList.add("btnGreen");
+	newForm.className = "link";
+	textArea.className = "input";
+	textArea.focus();
+	
+	btnSmall.addEventListener("click", nuevaEntrada);
+}
+function nuevaEntrada(){
+	this.parentElement.className = "dNone";
+	var entrada = document.createElement("div");
+	entrada.setAttribute("draggable", "true");
+	entrada.setAttribute("id", "move"+contador);
+	contador++;
+	entrada.innerText = this.previousElementSibling.value;
+	var padre = this.parentElement.parentElement;
+	padre.insertBefore(entrada, padre.lastChild);
+	entrada.className = "entrada";
+	padre.appendChild(entrada.previousSibling);
+	padre.lastChild.className = "link";
 
-	function nuevaCaja() {
-		var cajaDos = document.createElement("div");
-		cajaDos.className = "caja";
-		cajaGrande.appendChild(cajaDos);
-		span.className = "btn";
-		cajaDos.insertBefore(span, cajaDos.childNodes[0]);
-		cajaDos.insertBefore(form, cajaDos.childNodes[1]);
-	}
+	var cards = document.getElementsByClassName("entrada");
+		for (var i = 0, l = cards.length; i < l; i++) {
+			cards[i].addEventListener("dragstart", empiezaArrastrar);
+			//cards[i].addEventListener("dragenter", entraArrastrar);
+			//cards[i].addEventListener("dragleave", dejaArrastrar);
+			cards[i].addEventListener("dragend", terminaArrastrar);
+		}
+}
+function empiezaArrastrar(e) {
+	e.dataTransfer.setData("text", this.id);
+	this.style.opacity = "0.4";
+}
+function terminaArrastrar(e) {
+	this.style.opacity = null;
+}
 
-	function newText(padre){
-		var newForm = document.createElement("form");
-		var textArea = document.createElement("textarea");
-		var btnSmall = document.createElement("button");
-		
-		newForm.insertBefore(textArea, newForm.childNodes[0]);
-		newForm.insertBefore(btnSmall, newForm.childNodes[1]);
-		padre.appendChild(newForm);
-
-		btnSmall.innerText = "Añadir";
-		btnSmall.type = "button";
-		btnSmall.classList.add("btnGreen");
-		newForm.className = "link";
-		textArea.className = "input";
-		textArea.focus();
-		
-		btnSmall.addEventListener("click", function(){
-			newForm.className = "dNone";
-			nuevaEntrada(padre, textArea);
-		});
-	}
-	function nuevaEntrada(padre, textArea){
-		var entrada = document.createElement("div");
-		entrada.innerText = textArea.value;
-		padre.insertBefore(entrada, padre.lastChild);
-		entrada.className = "entrada";
-		padre.appendChild(entrada.previousSibling);
-		padre.lastChild.className = "link";
-	}
-});
+function arrastrarSobre(e) {
+	e.preventDefault();
+}
+function soltar(e) {
+	e.preventDefault();
+	var idArrastrado = e.dataTransfer.getData("text");
+	var elementoArrastrado = document.getElementById(idArrastrado);
+	this.insertBefore(elementoArrastrado, this.lastElementChild);
+}
